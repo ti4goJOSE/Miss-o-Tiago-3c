501908e926fba872ac87fe278fc15c8044ce0c0b
Tiago José <105284869+ti4goJOSE@users.noreply.github.com>
2024-06-06 10:32:37 -0300

    cs

---
+++ script.js
@@ -0,0 +1,16 @@
+const caixaprincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-pergunta")
+const caixaAlternativa = document.querySelector(".caixa-alternativa")
+const caixaresultado = document.querySelector(".caixa-resultado")
+const caixaResultado = document.querySelector(".texto-resultado")
+
+const perguntas = [
+    {//Primeiro objetivo da lista de perguntas
+        enunciado:"pergunta1",
+        Alternativas: ["alternativa1","alternativa2"]
+     }   
+     {//segundo objeto da lista de perguntas 
+        enunciado:"pergunta2",
+    Alternativas:["alternativa1","alternativa2"]
+  }
+];
